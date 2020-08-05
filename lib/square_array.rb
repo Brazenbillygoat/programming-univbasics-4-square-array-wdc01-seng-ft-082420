8cf98658dc879f651bd0b09dcce90949a104ac40
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-05 20:51:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,11 @@ def square_array(array)
   counter = 0 
   array_of_squares = []
   
-  while array[counter] {
+  while array[counter] do
     array_of_squares.push(array[counter] ** array[counter])
     counter += 1
-  }
+  end
  end
-  
+ 
+ return array_of_squares 
 end

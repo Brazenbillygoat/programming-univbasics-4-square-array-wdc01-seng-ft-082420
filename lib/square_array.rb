f4a350cc6bfe833f82de9ef1d1c1ee341c4efed9
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-05 20:28:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  array_of_squares = []
+  
+  while array[counter] {
+    array_of_squares.push(array[counter] ** array[counter])
+    counter += 1
+  }
+ end
+  
 end
